37379754269d6ad65c5212a5272c14d21c8449d2
camilondono <camilondono1216@gmail.com>
2022-10-23 22:24:27 -0400

    third commit

--- src/Class24/testergit.java
+++ src/Class24/testergit.java
@@ -11,5 +11,7 @@ public class testergit {
 
         System.out.println("Bandolero es mi zensei");
 
+        System.out.println("my third change in this code to be push by GitKraken Test test ");
+
     }
 }
